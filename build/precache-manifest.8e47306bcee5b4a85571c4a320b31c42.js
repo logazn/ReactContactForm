2d240a83f83f23d9a37b9a36bcf5a1dab5763487
Logan <loganfearon@gmail.com>
2019-02-01 08:20:57 +1300

    commit

---
+++ build/precache-manifest.8e47306bcee5b4a85571c4a320b31c42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b9c58657833d42104032",
+    "url": "/static/js/main.b9c58657.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "b9c58657833d42104032",
+    "url": "/static/css/main.282cd240.chunk.css"
+  },
+  {
+    "revision": "5be5732015823008e527562c365c83ba",
+    "url": "/index.html"
+  }
+];
