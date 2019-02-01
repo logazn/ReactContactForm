64165f426d8722bea5ba74b7ee5db0ced0a1c57c
Logan <loganfearon@gmail.com>
2019-02-01 23:17:57 +1300

    commit

---
+++ src/Components/Contact.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h4>John Doe</h4>
+        <ul>
+          <li>Email: John@gmail.com</li>
+          <li>Phone: 021029423</li>
+        </ul>
+      </div>
+    );
+  }
+}
+export default Contact;
